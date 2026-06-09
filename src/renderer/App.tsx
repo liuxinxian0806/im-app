0367c5c749b7d2908709b6351b79c0bf8dfa4a10
liuxx <liuxx@192.168.1.8>
2026-06-09 10:06:11 +0800

    init repo

---
+++ src/renderer/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-50">
+      <h1 className="text-3xl font-bold text-blue-600">
+        IM Desktop App Boilerplate Loaded!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
